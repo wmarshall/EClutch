757134ab73129addd28ae95105d3b57f2184fcf0
Will Marshall <willcodymarshall@gmail.com>
2015-12-29 16:16:09 -0500

    updated Eclutch to register itself

--- src/org/mort11/EClutch.java
+++ src/org/mort11/EClutch.java
@@ -3,48 +3,48 @@ package org.mort11;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class EClutch implements SpeedController {
-    private SpeedController wrappedController;
-    private boolean stallFlag = false;
-    private TemperatureIntegral tempIntegral = new TemperatureIntegral();
+	private SpeedController wrappedController;
+	private boolean stallFlag = false;
+	private TemperatureIntegral tempIntegral = new TemperatureIntegral();
 
-    private double setpoint;
+	private double setpoint;
 
-    public EClutch(int PDPChannel, SpeedController wrappedController) throws Exception {
-        this.wrappedController = wrappedController;
-        ClutchThread.getInstance().registerMotor(wrappedController, PDPChannel);
-    }
+	public EClutch(int PDPChannel, SpeedController wrappedController) throws Exception {
+		this.wrappedController = wrappedController;
+		ClutchThread.getInstance().registerMotor(this, PDPChannel);
+	}
 
-    @Override
-    public double get() {
-        return wrappedController.get();
-    }
+	@Override
+	public double get() {
+		return wrappedController.get();
+	}
 
-    @Override
-    public void set(double speed, byte syncGroup) {
-        this.setpoint = speed;
-        if (isStalled()) {
-            wrappedController.set(0);
-        } else {
-            wrappedController.set(setpoint);
-        }
-    }
+	@Override
+	public void set(double speed, byte syncGroup) {
+		this.setpoint = speed;
+		if (isStalled()) {
+			wrappedController.set(0);
+		} else {
+			wrappedController.set(setpoint);
+		}
+	}
 
-    @Override
-    public void set(double speed) {
-        set(speed, (byte) 0);
-    }
+	@Override
+	public void set(double speed) {
+		set(speed, (byte) 0);
+	}
 
-    @Override
-    public void disable() {
-        wrappedController.disable();
-    }
+	@Override
+	public void disable() {
+		wrappedController.disable();
+	}
 
-    @Override
-    public void pidWrite(double output) {
+	@Override
+	public void pidWrite(double output) {
 
-    }
+	}
 
-    public boolean isStalled() {
-        return this.stallFlag;
-    }
+	public boolean isStalled() {
+		return this.stallFlag;
+	}
 }
